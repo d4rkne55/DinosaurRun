f41bf64c3ddd90699e33d14572854360060f8f68
Dennis Jungbauer <djungbauer@chip.de>
2018-07-03 23:18:57 +0200

    added some javadoc for helpers

--- src/dinosaurGame/helper/RandomNumber.java
+++ src/dinosaurGame/helper/RandomNumber.java
@@ -12,18 +12,37 @@ public final class RandomNumber
      */
     private static Random rand = new Random();
     
+    /**
+     * Generates a random full-range integer, from Integer.MIN_VALUE to Integer.MAX_VALUE
+     */
     public static int getInt() {
         return rand.nextInt();
     }
     
+    /**
+     * Generates a random integer between given min (inclusive) and max (inclusive)
+     * 
+     * @param min
+     * @param max
+     */
     public static int getIntRange(int min, int max) {
         return min + rand.nextInt(max + 1 - min);
     }
     
+    /**
+     * Generates a random float from 0.0 (inclusive) to 1.0 (exclusive)
+     */
     public static float getFloat() {
         return rand.nextFloat();
     }
     
+    /**
+     * Generates a random float between given min (inclusive) and max (exclusive)
+     * TODO: try to get max inclusive
+     * 
+     * @param min
+     * @param max
+     */
     public static float getFloatRange(float min, float max) {
         return min + rand.nextFloat() * (max - min);
     }
--- src/dinosaurGame/helper/ResourceLoader.java
+++ src/dinosaurGame/helper/ResourceLoader.java
@@ -15,6 +15,13 @@ public class ResourceLoader
 {
     private HashMap<String, Object> resources = new HashMap<String, Object>();
     
+    /**
+     * Loads and stores an image to the cache
+     * 
+     * @param name
+     * @param imgPath
+     * @return {@code true} when loaded and stored, {@code false} when the requested image couldn't be loaded
+     */
     public boolean loadImage(String name, String imgPath) {
         BufferedImage img = loadImage(imgPath);
         
@@ -26,14 +33,27 @@ public class ResourceLoader
         }
     }
     
+    /**
+     * Gets stored image from cache
+     * 
+     * @param name
+     */
     public BufferedImage getImage(String name) {
         return (BufferedImage) this.resources.get(name);
     }
     
+    /**
+     * Returns a Set with all the names/keys of stored resources
+     */
     public Set<String> getNames() {
         return this.resources.keySet();
     }
     
+    /**
+     * Static function for just loading and returning the requested image. No use of cache.
+     * 
+     * @param imgPath
+     */
     public static BufferedImage loadImage(String imgPath) {
         try {
             return ImageIO.read(new File(imgPath));
@@ -43,6 +63,11 @@ public class ResourceLoader
         }
     }
     
+    /**
+     * Static function for loading and registering a custom font
+     * 
+     * @param filePath
+     */
     public static void registerFont(String filePath) {
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         
